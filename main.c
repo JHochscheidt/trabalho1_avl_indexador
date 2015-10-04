e25018deefe8c9d1479522b534279365a09d37fc
Jackson Hochscheidt <jackson94h@gmail.com>
2015-10-04 13:21:13 -0300

    ta funcionando pohaaaaa

--- main.c
+++ main.c
@@ -5,21 +5,32 @@ int main(){
 
 	TpArvore *AVL = criaArvoreVazia();
 
-	inserir(20, AVL->root, AVL);
+	inserir(10, AVL->root, AVL);
 	//imprimir(AVL->root);
 	//puts("\n");
-	inserir(70, AVL->root, AVL);
+	inserir(20, AVL->root, AVL);
 	imprimir(AVL->root);
 	//imprimir(AVL->root);
 	puts("\n");
-	inserir(25, AVL->root, AVL);
+	inserir(30, AVL->root, AVL);
 	imprimir(AVL->root);
-	inserir(100, AVL->root, AVL);
+	inserir(40, AVL->root, AVL);
+	inserir(50, AVL->root, AVL);
+	inserir(60, AVL->root, AVL);
+	inserir(70, AVL->root, AVL);
 	inserir(80, AVL->root, AVL);
+	inserir(90, AVL->root, AVL);
+	inserir(100, AVL->root, AVL);
 	inserir(110, AVL->root, AVL);
+	inserir(120, AVL->root, AVL);
+	inserir(130, AVL->root, AVL);
+	inserir(140, AVL->root, AVL);
+	inserir(150, AVL->root, AVL);
+	
 	puts("\n");
 	imprimir(AVL->root);
 
+	puts("\n");
 	printf("altura da arvore %d", altura(AVL->root));
 	/*inserir(30, AVL->root, AVL);
 	inserir(10, AVL->root, AVL);
