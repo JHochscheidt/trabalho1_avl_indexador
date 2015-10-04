5b8ec2706ed51556a2e69bd0a028ccdf55f75c90
Jackson Hochscheidt <jackson94h@gmail.com>
2015-10-04 17:58:00 -0300

    tudo ai

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "manArq.h"
 
 int main(int argc, char **argv){
-
+	/*
 	TpArvore *teste = criaArvoreVazia();
 	inserir("abacate", teste->root, teste);
 	inserir("aba", teste->root, teste);
@@ -11,16 +11,20 @@ int main(int argc, char **argv){
 	inserir("aacate", teste->root, teste);
 	inserir("bacate", teste->root, teste);
 	imprimir(teste->root);
-
-	/*
-
-
-
+	*/
+	// -- criar arvore aqui
+		// criar arvore de A até E
+		TpArvore *AateE = criaArvoreVazia();
+		// criar arvore de F até J
+		TpArvore *FateJ = criaArvoreVazia();
+		// criar arvore de K ate O
+		TpArvore *KateO = criaArvoreVazia();
+		// criar arvore de P até T
+		TpArvore *PateT = criaArvoreVazia();
+		// criar arvore de U até Z
+		TpArvore *UateZ = criaArvoreVazia();
 	
-	//char **arquivos; // para armazenar o nome dos arquivos
-	//int i = 0;
 	int op;
-	//char *aux;
 	do{
 		menu();
 		scanf("%d", &op);
@@ -37,26 +41,20 @@ int main(int argc, char **argv){
 					pFileTable = fopen(argv[1], "r");
 					if(pFileTable == NULL)
 						perror("ARQUIVO DE ENTRADA VAZIO\n");
-
-					//FILE *pFile = NULL;
-					//char buffer[BUFFLEN];
+						
 					int nFiles;
 					unsigned len;
 					char *nomeArq = (char*) malloc(sizeof(char)); // para armazenar temporariamente o nome de cada arquivo
 					char *quantArquivos = (char*) malloc(sizeof(char)); // necessario para poder guardar a quant de arquivos
 					int i = 0;
-					int j=0;
-						
+					int j = 0;
+									
 					fgets(quantArquivos, 100, pFileTable); // pega a primeira linha do arquivo, que é o numero de arquivos
 					nFiles = atoi(quantArquivos);
 					printf("\nquantidade de arquivos >>>> %d \n", nFiles);
 					// ok ate aqui
-					
-					char *files[nFiles];
-					//fseek(pFileTable, 1, SEEK_CUR);// posiciona 1 byte p frente forçando a descer a linha
+					char *files[nFiles];  //fseek(pFileTable, 1, SEEK_CUR);// posiciona 1 byte p frente forçando a descer a linha
 					//ok ate aqui
-
-
 					while(fgets(nomeArq,100,pFileTable) != NULL && i < nFiles){// copia os nomes dos arquivos
 						//printf("arquivo >>>[%s]\n", nomeArq);
 						//if(validNameFile(nomeArq)){ // fazer essa verificacao na valida nome, verificar tbm a ajustaStr
@@ -70,62 +68,39 @@ int main(int argc, char **argv){
 						//}
 					}// nesse ponto files é um vetor de nomes válidos de arquivos a serem indexados (o ID de cada arquivo é sua posição em files + 1, ou seja i+1)
 					fclose(pFileTable);
+					/* 	int cont;
+						printf("\nimprimindo o nome dos arquivos >>\n");
+						for(cont = 0; cont < nFiles; cont++){ printf("[%s]\n", files[cont]); }
 					*/
-					/*
-					int cont;
-					printf("\nimprimindo o nome dos arquivos >>\n");
-					for(cont = 0; cont < nFiles; cont++){ printf("[%s]\n", files[cont]); }
-					*/
-					// ok ate aqui. Unico problema é q quando imprime o nome dos arquivos esta imprimindo lixo
-					/*
-					
-					// -- criar arvore aqui
-						// criar arvore de A até E
-						TpArvore *AateE = criaArvoreVazia();
-						// criar arvore de F até J
-						//TpArvore *FateJ = criaArvoreVazia();
-						// criar arvore de K ate O
-						//TpArvore *KateO = criaArvoreVazia();
-						// criar arvore de P até T
-						//TpArvore *PateT = criaArvoreVazia();
-						// criar arvore de U até Z
-						//TpArvore *UateZ = criaArvoreVazia();
-					
-						printf("analisando arquivo >>");
-						for(j=0;j < i;j++){// analisa 1 arquivo por vez
-							printf("[%s]\n", files[j]);
-							if(files[j] == NULL)
-								continue;
-
-							FILE *pFile = fopen(files[j], "r");
-							
-							if(pFile == NULL){
-								puts("arq n encontrado");
-								continue;
-							}
-							fseek(pFile, 0, SEEK_END); // posiciona para o final do arquivo
-							int tamanhoArquivo = ftell(pFile); // retorna a ultima posicao do arquivo, ou seja o tamanho dele
-							rewind(pFile); // volta arquivo para o começo
-							char *buffer = (char*) malloc(sizeof(char) * tamanhoArquivo); // buffer do tamanho do arquivo
-
-							puts("aqui");
-							while(fgets(buffer,tamanhoArquivo,pFile) != NULL){//pega por linha ou o arquivo inteiro
-								//analisar aqui e por na arvore a ocorrencia junto com o ID do arq 
-								printf("antes len:%d\n", (int)strlen(buffer));
-								
-								ajustaStr(buffer);*/ /* feito, ta sem o \n no fim da string. 
+					printf("analisando arquivo >>");
+					for(j=0;j < i;j++){// analisa 1 arquivo por vez
+						printf("[%s]\n", files[j]);
+						if(files[j] == NULL)
+							continue;
+						FILE *pFile = fopen(files[j], "r");
+						if(pFile == NULL){
+							puts("arq n encontrado");
+							continue;
+						}
+						fseek(pFile, 0, SEEK_END); // posiciona para o final do arquivo
+						int tamanhoArquivo = ftell(pFile); // retorna a ultima posicao do arquivo, ou seja o tamanho dele
+						rewind(pFile); // volta arquivo para o começo
+						char *buffer = (char*) malloc(sizeof(char) * tamanhoArquivo); // buffer do tamanho do arquivo
+						puts("aqui");
+						while(fgets(buffer,tamanhoArquivo,pFile) != NULL){//pega por linha ou o arquivo inteiro
+							//analisar aqui e por na arvore a ocorrencia junto com o ID do arq 
+							printf("antes len:%d\n", (int)strlen(buffer));	
+							ajustaStr(buffer); /*feito, ta sem o \n no fim da string. 
 													Temos que olhar p ela como se fosse uma sequencia de palavras independente da iteração
 													* Ou seja, uma palavra pode começar em uma iteração e terminar em outra, abc*/
-								/*
-								printf("dps len:%d\n", (int)strlen(buffer));
-
-								char *pch;
-								printf ("%s  em tokens: \n Splitting seqüência \n", buffer);
-								pch = strtok (buffer, ", .-?!,.:;");
-								if(strncmp(pch, "A", 1) == 0 || strncmp(pch, "a", 1) == 0 || strncmp(pch, "B", 1) == 0 || strncmp(pch, "b", 1) == 0 || strncmp(pch, "C", 1) == 0 || strncmp(pch, "c", 1) == 0 || strncmp(pch, "D", 1) == 0 || strncmp(pch, "d", 1) == 0 || strncmp(pch, "E", 1) == 0 || strncmp(pch, "e", 1) == 0){
-										inserir(pch, AateE->root, AateE);
-										imprimir(AateE->root);
-								}
+							printf("dps len:%d\n", (int)strlen(buffer));
+							char *pch;
+							printf ("%s  em tokens: \n Splitting seqüência \n", buffer);
+							pch = strtok (buffer, ", .-?!,.:;");
+							if(strncmp(pch, "A", 1) == 0 || strncmp(pch, "a", 1) == 0 || strncmp(pch, "B", 1) == 0 || strncmp(pch, "b", 1) == 0 || strncmp(pch, "C", 1) == 0 || strncmp(pch, "c", 1) == 0 || strncmp(pch, "D", 1) == 0 || strncmp(pch, "d", 1) == 0 || strncmp(pch, "E", 1) == 0 || strncmp(pch, "e", 1) == 0){
+								inserir(pch, AateE->root, AateE);
+								imprimir(AateE->root);
+							}
 								while (pch != NULL){
 									printf ("%s \n", pch);
 									*//*if(strncmp(pch, "A", 1) == 0 || strncmp(pch, "a", 1) == 0 || strncmp(pch, "B", 1) == 0 || strncmp(pch, "b", 1) == 0 || strncmp(pch, "C", 1) == 0 || strncmp(pch, "c", 1) == 0 || strncmp(pch, "D", 1) == 0 || strncmp(pch, "d", 1) == 0 || strncmp(pch, "E", 1) == 0 || strncmp(pch, "e", 1) == 0){
